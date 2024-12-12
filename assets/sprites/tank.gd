e32d9392cb352e0306bfa407fad26a652f66b0f3
pedohorse <13556996+pedohorse@users.noreply.github.com>
2024-12-12 21:46:42 +0100

    add name and log labels

--- assets/sprites/tank.gd
+++ assets/sprites/tank.gd
@@ -1,8 +1,24 @@
 extends Node2D
+class_name Tank
 
 @onready var body: Sprite2D = $body as Sprite2D
 @onready var tower: Sprite2D = $tower as Sprite2D
+@onready var name_label: Label = $name as Label
+@onready var logbox: Container = $logbox as Container
 
 func set_color(color: Color):
 	body.modulate = color
 	tower.modulate = color
+
+func set_tank_name(name: String):
+	name_label.text = name
+
+func push_log_line(line: String):
+	var logline = Label.new()
+	logline.text = line
+	logbox.add_child(logline)
+	var tween = logline.create_tween()
+	tween.tween_callback(
+		func():
+			logline.queue_free()
+	).set_delay(10)
